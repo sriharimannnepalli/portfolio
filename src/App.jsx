a6f79992131218c8986ee055a32c61e675883e26
sriharimannepalli <sriharimannepalli48@gmail.com>
2025-03-17 19:02:21 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const App = () => {
+  return (
+    <NavBar/>
+  )
+}
+
+export default App
---
+++ src/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <div className='outer'>
+        <div className='name'>
+            <p>SRIHARI MANNEPALLI</p> 
+        </div>
+
+        <ul className='inner'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Qualifications</li>
+        <li>Projects</li>
+        <li>Contacte</li>
+        </ul>
+
+        <div className='button'>
+        <button >LetsTalk</button>
+        </div>
+        <img src="https://api.iconify.design/material-symbols:partly-cloudy-day-rounded.svg" alt="" />
+
+    </div>
+  )
+}
+
+export default NavBar
